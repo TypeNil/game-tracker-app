ee2e7519927ed7c798ecb308b08e7cce5211bd80
typenil <typenil0@yahoo.com>
2026-09-01 07:20:04 +0300

    test(search): remove unused onFirst import in SearchScreenTest

--- app/src/androidTest/java/io/github/typenil/gametracker/feature/search/SearchScreenTest.kt
+++ app/src/androidTest/java/io/github/typenil/gametracker/feature/search/SearchScreenTest.kt
@@ -4,7 +4,6 @@ import androidx.activity.ComponentActivity
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onAllNodesWithText
-import androidx.compose.ui.test.onFirst
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.onNodeWithTag
